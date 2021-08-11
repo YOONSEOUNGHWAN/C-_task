be376017bf4991e72c704094a32e28f37df24904
yoon <seoung59@gmail.com>
2021-08-11 21:14:59 +0900

    Initial Commit

---
+++ C++_task/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++_task
+//
+//  Created by YOON on 2021/08/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
