a9b307d61cd895a01761dcf33a1d4217822574a6
yoon <seoung59@gmail.com>
2021-08-11 21:43:27 +0900

    Subsets

--- C++_task/main.cpp
+++ C++_task/main.cpp
@@ -7,8 +7,21 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+using namespace std;
+void printSubsets(char arr[], int n){
+    for(int i=0;i<(1<<n);i++){
+        cout<<"{";
+        for (int j=0; j<n; j++){
+            if(i & (1 << j))
+                cout<<arr[j]<<" ";
+        }
+        cout<<"}"<<endl;
+    }
+}
+
+int main(){
+    char data[] = {'A','B','C','D'};
+    printSubsets(data, 4);
     return 0;
 }
